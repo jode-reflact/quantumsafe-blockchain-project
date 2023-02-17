1bdfaaa0ad03e9f4226f413f843a6550ebfab50f
Jonas Deterding <jonas.deterding@reflact.com>
2023-02-17 21:27:21 +0100

    init Node Evaluation server, receives test results and inserts them into db

---
+++ evaluation_server/src/Server.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response } from 'express';
+import { Collection, Db, MongoClient } from 'mongodb';
+import bodyParser from 'body-parser';
+
+export type TestResult = any;
+
+export class EvaluationServer {
+    public app = express();
+    public mainDb: Db
+    public testResultsCol: Collection<TestResult>
+
+    constructor() {
+        this.initDB()
+        this.initExpress()
+    }
+    private async initDB() {
+        const mongoClient = await MongoClient.connect("mongodb://" + process.env.dbuser + ":" + process.env.dbpass + "@localhost:27017/admin");
+        this.mainDb = mongoClient.db(process.env.dbname);
+        this.testResultsCol = this.mainDb.collection("testResults");
+    }
+    private async initExpress() {
+        this.app.use(bodyParser.json({ limit: "100mb" }));
+        await this.app.listen(80)
+
+        this.app.post("/completed_test", [], async (req: Request, res: Response) => {
+            const testResult: TestResult = req.body;
+            await this.testResultsCol.insertOne(testResult);
+            // TODO: end test
+        })
+    }
+}
