68ffdcc3b6a0b010af570ff4c79648f2cdf74d6e
Noah Janzen <noah.janzen@hs-ruhrwest.de>
2023-02-27 12:26:48 +0100

    Stores str representation of pending tx in db

--- node/transaction/pending_transaction_model.py
+++ node/transaction/pending_transaction_model.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from node.database import db
 from node.transaction.base_transaction_model import BaseTransaction
 
@@ -10,6 +12,22 @@ class PendingTransaction(db.Model, BaseTransaction):
     amount = db.Column(db.String)
     signature = db.Column(db.String)
     receivedAt = db.Column(db.String)
+    cached_representation = db.Column(db.String)
+
+    def __init__(self, timestamp, sender, receiver, amount, signature, receivedAt):
+        self.timestamp = timestamp
+        self.sender = sender
+        self.receiver = receiver
+        self.amount = amount
+        self.signature = signature
+        self.receivedAt = receivedAt
+        self.cached_representation = BaseTransaction.build_str_representation(
+            sender=sender,
+            receiver=receiver,
+            amount=amount,
+            timestamp=timestamp,
+            signature=signature,
+        )
 
     @staticmethod
     def from_json(json):
@@ -30,3 +48,13 @@ class PendingTransaction(db.Model, BaseTransaction):
                 amount= str(json["amount"]),
                 signature=json["signature"],
             )
+
+    @staticmethod
+    def build_str_representation(sender: str, receiver: str, amount: str, timestamp: str, signature: str) -> str:
+        return str(OrderedDict({
+            "sender": sender,
+            "receiver": receiver,
+            "amount": amount,
+            "timestamp": timestamp,
+            "signature": signature
+        }))
